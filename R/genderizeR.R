3c834d9bf78f2ac307edbc97eaa44ad6bf2f9aa2
kalimu <kamil.wais@gmail.com>
2014-12-11 06:18:49 +0100

    package help index file

--- R/genderizeR.R
+++ R/genderizeR.R
@@ -11,4 +11,26 @@
     packageStartupMessage("See: citation('genderizeR')")
 
 }
+
+#' An R package for predicting gender from first names
+#'
+#' The \code{genderizeR} package uses genderize.io API to predict gender from first names extracted from text corpuses. The accuracy of prediction could be control by two parameters: counts of first names in database and probability of gender given the first name. 
+#'
+#'
+#' @docType package
+#' 
+#' @name genderizeR
+#' 
+#' @seealso 
+#' \itemize{
+#'   \item \url{https://github.com/kalimu/genderizeR} [R package source code]
+#'   \item \url{http://www.wais.kamil.rzeszow.pl/genderizeR} [R package homepage]
+#' }
+#' 
+#' 
+#@keywords internal
+ 
+NULL
+ 
 # detach("package:genderizeR", unload=TRUE)
+# devtools::show_news()

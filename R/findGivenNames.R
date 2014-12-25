27d37cc274a487df8ca142165dc24c93f3d092f2
kalimu <kamil.wais@gmail.com>
2014-12-25 12:46:37 +0100

    fix

--- R/findGivenNames.R
+++ R/findGivenNames.R
@@ -83,7 +83,7 @@ findGivenNames = function (x, queryLength = 400, distributedCorpus = FALSE) {
                )
         )
  
-        pb   <- txtProgressBar(0, nPackages, style=3)
+        pb   <- txtProgressBar(0, nPackages, style=3, width=43)
         setTxtProgressBar(pb, p)
  
 

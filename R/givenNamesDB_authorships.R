b8a995b0f6befc16e4eaf406183f2fbde4f387cb
kalimu <kamil.wais@gmail.com>
2015-01-10 09:10:29 +0100

    archive datasets with givennames for authorship sample

---
+++ R/givenNamesDB_authorships.R
@@ -0,0 +1,22 @@
+#' Gender data for authorship sample
+#'
+#' A dataset with a gender data from genderize.io for the sample 
+#' of \pkg{authorships} in this package. This is the output 
+#' of \code{findGivenNames} 
+#' function ran on December 26, 2014. 
+#'
+#' @format A data.table object with 872 rows and 4 variables:
+#' \describe{
+#'   \item{name}{first name}
+#'   \item{gender}{predicted gender}
+#'   \item{probability}{how many persons in with this first name 
+#'   has the predicted gender}
+#'   \item{count}{how many persons in the genderize.io database 
+#'   had that first name}
+#' }
+#' @source \url{http://genderize.io/}
+#' 
+"givenNamesDB_authorships"
+
+# givenNamesDB_authorships = readRDS("data-raw/givenNamesDB_authorships.rds")
+# devtools::use_data(givenNamesDB_authorships)

1bb7301e5b37913a42469f76b94c1773d04630ee
kalimu <kamil.wais@gmail.com>
2015-01-14 08:27:56 +0100

    doc for bootstrap func

--- R/genderizeBootstrapError.R
+++ R/genderizeBootstrapError.R
@@ -1,7 +1,6 @@
-#' Training genderize function
+#' Gender prediction errors on bootstrap samples
 #' 
-#' \code{genderizeTrain} predicts gender and checks different combination
-#' of 'probability' and 'count' paramters. 
+#' \code{genderizeBootstrapError} calculates the Apparent Error Rate, the Leave-One-Out bootstrap error rate  and the .632+ error rate from Efron and Tibishirani (1997). The code is modified version of several functions from \code{sortinghat} package by John A.Ramey.
 #' 
 #' 
 #' @param x A text vector that we want to genderize
@@ -11,16 +10,14 @@
 #' Used to subseting a givenNamesDB dataset
 #' @param counts A numeric vector of different count values. 
 #' Used to subseting a givenNamesDB dataset
-#' @param parallel If TRUE it computes errors with the use of \code{parallel} package and available cores. It is design to work on windows machines. Default is FALSE.
+#' @param parallel It is passed to \code{genderizeTrain} function. If TRUE it computes errors with the use of \code{parallel} package and available cores. It is design to work on windows machines. Default is FALSE.
 #' 
-#' @return A data frame with all combination of parameters and computed 
-#' sets of prediction indicators for each combination:
-#'   \item{errorCoded}{classification error for predictet & unpredicted gender}
-#'   \item{errorCodedWithoutNA}{for predicted gender only}
-#'   \item{naCoded}{of manually coded gender only }
-#'   \item{errorGenderBias}{net gender bias error}
+#' @return A list of bootstrap errors:
+#'   \item{apparent}{Apparent Error Rate}
+#'   \item{loo_boot}{LOO-Boot Error Rate}
+#'   \item{errorRate632plus}{.632+ Error Rate}
 #'   
-#' @seealso Implementation of parallel mclapply on Windows machines by Nathan VanHoudnos \link{http://www.stat.cmu.edu/~nmv/setup/mclapply.hack.R}
+#' @seealso In the \code{sortinghat} package: \code{\link[sortinghat]{errorest_apparent}} \code{\link[sortinghat]{errorest_loo_boot}} \code{\link[sortinghat]{errorest_632plus}} 
 #' 
 #' @examples 
 #' \dontrun{
@@ -29,100 +26,66 @@
 #'
 #' }
 
-genderizeBootstrapError = function(x, 
-                          y, 
+genderizeBootstrapError = function(x, y, 
                           givenNamesDB,
-                          probs,
-                          counts,
+                          probs, counts,
                           num_bootstraps = 50,
                           parallel = TRUE
                           ){
     
-
-    
-    # source('http://www.stat.cmu.edu/~nmv/setup/mclapply.hack.R')
-    #source('~/# Rscripts/WebOfScience/mclapply.hack.R')
-    # http://www.stat.cmu.edu/~nmv/2014/07/14/implementing-mclapply-on-windows/
-    
-      
     givenNamesDB = data.table::as.data.table(givenNamesDB)
-
-   
-    writeLines(c("starting bootstraping..."), "bootstraping.log")
-#     
-#   if (parallel == TRUE) {
-    loo_boot_error_rates <- lapply(
-    #loo_boot_error_rates <- mclapply.hack(
-                                 seq_len(num_bootstraps), function(b) {
-        
-        training <- sample(seq_y, replace = TRUE)
-        
-        test <- which(!(seq_y %in% training))
-        
-        trainedParams = genderizeTrain(x=x[training], y=y[training], 
-                              givenNamesDB = givenNamesDB, 
-                              probs = probs, counts = counts, parallel=parallel)
-        trainedParams
-        
-        classifications = genderizePredict(trainedParams, 
-                                   newdata=x[test],
-                                   givenNamesDB = givenNamesDB)
-        
-        # mean(classifications != y[test])
-        
-        sink("bootstraping.log", append = TRUE)
-        
-            # b=23
-            cat(paste0('[',num_bootstraps,']: ', b,'\n'))
-        
-        sink()  # empty the sink stack
-        
-        print(b)
-        
-        replace(rep_NA, test, classifications != y[test])
-        
-    })
     
-    loo_boot_error_rates_backup = loo_boot_error_rates
-    # loo_boot_error_rates = loo_boot_error_rates_backup
+    writeLines(c("starting bootstraping..."), "bootstraping.log")
+    
+    loo_boot_error_rates <- 
+        
+        lapply(seq_len(num_bootstraps), function(b) {
+            
+            training <- sample(seq_y, replace = TRUE)
+            test <- which(!(seq_y %in% training))
+            
+            trainedParams = genderizeTrain(x=x[training], y=y[training],
+                                           givenNamesDB = givenNamesDB,
+                                           probs = probs, counts = counts, 
+                                           parallel=parallel)
+            
+            classifications = genderizePredict(trainedParams,
+                                               newdata=x[test],
+                                               givenNamesDB = givenNamesDB)
+            
+            sink("bootstraping.log", append = TRUE)
+            
+                cat(paste0('[',num_bootstraps,']: ', b,'\n'))
+            
+            sink()  # empty the sink stack
+            
+            print(b)
+            
+            replace(rep_NA, test, classifications != y[test])
+            
+        })
     
     loo_boot_error_rates <- do.call(rbind, loo_boot_error_rates)
-    # loo_boot_error_rates[1,437] = TRUE
-    
     loo_boot_error_rates <- colMeans(loo_boot_error_rates, na.rm = TRUE)
     
     if (any(is.nan(loo_boot_error_rates))) {
+        
         warning("Some observations were not included as test observations. Returning 'NaN'")
+        
     }
     
-    (loo_boot = mean(loo_boot_error_rates))
-#  
-  
-  
-  
-     trainedParams = genderizeTrain(x=x, y=y, 
-                          givenNamesDB = givenNamesDB, 
-                          probs = probs, counts = counts,  parallel=parallel)
- 
-
-  
-
+    loo_boot = mean(loo_boot_error_rates)
     
-    classifications = genderizePredict(trainedParams = trainedParams, 
-                               newdata = x, 
-                               givenNamesDB  = givenNamesDB)  
-
-
-# classificatonErrors(labels = y, predictions = classifications)
-#     
-#     table(y,classifications, useNA = 'always')
-    apparent= mean(y != classifications)
-
-
-
-
+    trainedParams = genderizeTrain(x=x, y=y, 
+                                   givenNamesDB = givenNamesDB, 
+                                   probs = probs, counts = counts,  
+                                   parallel=parallel)
     
-    classify_out = classifications 
+    classify_out = genderizePredict(trainedParams = trainedParams, 
+                                       newdata = x, 
+                                       givenNamesDB  = givenNamesDB)  
+    
+    apparent= mean(y != classify_out)
     
     n <- length(y)
     tab = table(y)
@@ -136,19 +99,23 @@ genderizeBootstrapError = function(x,
     R_hat <- (loo_boot - apparent)/(gamma_hat - apparent)
     w_hat <- 0.632/(1 - 0.368 * R_hat)
     
-
-
- list(   
-    apparent = apparent,
-    loo_boot = loo_boot,
     errorRate632plus = ((1 - w_hat) * apparent + w_hat * loo_boot)
+    
+    list(   
+        apparent = apparent,
+        loo_boot = loo_boot,
+        errorRate632plus = errorRate632plus
+        )
   
- )
-  
-#   }
+}
   
     
-#  
+
+
+#      # source('http://www.stat.cmu.edu/~nmv/setup/mclapply.hack.R')
+    #source('~/# Rscripts/WebOfScience/mclapply.hack.R')
+    # http://www.stat.cmu.edu/~nmv/2014/07/14/implementing-mclapply-on-windows/
+    
 # # 
 # #    set.seed(42)
 # # 
@@ -264,7 +231,7 @@ genderizeBootstrapError = function(x,
 #     
 #     return(loo_boot = mean(loo_boot_error_rates))
     
-}
+
 
     
    

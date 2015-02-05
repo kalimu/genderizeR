7662225fe14afcc0dec75533e7cea36dbb40a654
kalimu <kamil.wais@gmail.com>
2015-02-05 10:47:01 +0100

    without distributedCorpus and docs update

--- R/textPrepare.R
+++ R/textPrepare.R
@@ -5,9 +5,9 @@
 #' 
 #' @param x A vector of character strings.
 #' @param textPrepMessages If TRUE verbose output of the prepairing process is shown on the console.
-#' @param distributedCorpus If TRUE use as.DistributedCorpus from the tm.plugin.dc package.
+ 
 #'
-#' @return frequent terms vector.
+#' @return A vector of unique terms
 #' 
 #' 
 #' 
@@ -26,7 +26,7 @@
 #' 
 #' @export
 
-textPrepare = function (x, textPrepMessages = FALSE, distributedCorpus = FALSE) {
+textPrepare = function (x, textPrepMessages = FALSE ) {
 
 
     if (textPrepMessages == TRUE) cat('removing special characters...\n')    
@@ -39,11 +39,9 @@ textPrepare = function (x, textPrepMessages = FALSE, distributedCorpus = FALSE) 
 
     if (textPrepMessages == TRUE) cat('building text-mining corpus...\n')    
     
-    if (!distributedCorpus) {
+ 
         x = tm::Corpus(tm::VectorSource(x))
-    } else {
-        x = tm.plugin.dc::as.DistributedCorpus(tm::VCorpus(tm::VectorSource(x)))
-    }
+ 
 
     if (textPrepMessages == TRUE) cat('building term matrix...\n')   
     if (textPrepMessages == TRUE) cat('removing abbreviations...\n')  

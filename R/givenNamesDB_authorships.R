fd4e486b9b7c3e86ec05cd495c1df64c57ff3f5f
kalimu <kamil.wais@gmail.com>
2015-02-05 09:51:12 +0100

    docs update

--- R/givenNamesDB_authorships.R
+++ R/givenNamesDB_authorships.R
@@ -1,9 +1,9 @@
 #' Gender data for authorship sample
 #'
-#' A dataset with a gender data from genderize.io for the sample 
+#' A dataset with first names and gender data from genderize.io for the sample 
 #' of \pkg{authorships} in this package. This is the output 
 #' of \code{findGivenNames} 
-#' function ran on December 26, 2014. 
+#' function that was performed on December 26, 2014. 
 #'
 #' @format A data.table object with 872 rows and 4 variables:
 #' \describe{
